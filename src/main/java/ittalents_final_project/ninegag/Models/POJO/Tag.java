513e606017b27fe91ec2811d229073901ad339a5
Robsan96 <robsan96@abv.bg>
2019-02-28 15:28:00 +0200

    Adding some POJO and DAO classes

---
+++ src/main/java/ittalents_final_project/ninegag/Models/POJO/Tag.java
@@ -0,0 +1,17 @@
+package ittalents_final_project.ninegag.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tag {
+
+    int tag_id;
+    String name;
+
+}
